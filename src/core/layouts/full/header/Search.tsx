9953554111ba55c4dc324a1264de8ca257152799
Dazza <adaza90@outlook.com>
2025-11-15 21:15:25 -0500

    Update Search.tsx

--- src/core/layouts/full/header/Search.tsx
+++ src/core/layouts/full/header/Search.tsx
@@ -5,45 +5,17 @@ import {
   DialogContent,
   Stack,
   Divider,
-  Box,
-  List,
-  ListItemText,
-  Typography,
   TextField,
-  ListItemButton,
 } from "@mui/material";
 import { IconSearch, IconX } from "@tabler/icons-react";
-import Menuitems from "../sidebar/MenuItems";
-import { Link } from "react-router";
-
-interface menuType {
-  title: string;
-  id: string;
-  subheader: string;
-  children: menuType[];
-  href: string;
-}
 
 const Search = () => {
-  // drawer top
   const [showDrawer2, setShowDrawer2] = useState(false);
   const [search, setSearch] = useState("");
 
   const handleDrawerClose2 = () => {
     setShowDrawer2(false);
   };
-
-  const filterRoutes = (rotr: any, cSearch: string) => {
-    if (rotr.length > 1)
-      return rotr.filter((t: any) =>
-        t.title
-          ? t.href.toLocaleLowerCase().includes(cSearch.toLocaleLowerCase())
-          : ""
-      );
-
-    return rotr;
-  };
-  const searchData = filterRoutes(Menuitems, search);
 
   return (
     <>
@@ -81,60 +53,9 @@ const Search = () => {
               <IconX size="18" />
             </IconButton>
           </Stack>
+          <Divider />
+          <p>Search results for: {search}</p>
         </DialogContent>
-        <Divider />
-        <Box p={2} sx={{ maxHeight: "60vh", overflow: "auto" }}>
-          <Typography variant="h5" p={1}>
-            Quick Page Links
-          </Typography>
-          <Box>
-            <List component="nav">
-              {searchData.map((menu: menuType) => {
-                return (
-                  <Box key={menu.title ? menu.id : menu.subheader}>
-                    {menu.title && !menu.children ? (
-                      <ListItemButton
-                        sx={{ py: 0.5, px: 1 }}
-                        to={menu?.href}
-                        component={Link}
-                      >
-                        <ListItemText
-                          primary={menu.title}
-                          secondary={menu?.href}
-                          sx={{ my: 0, py: 0.5 }}
-                        />
-                      </ListItemButton>
-                    ) : (
-                      ""
-                    )}
-                    {menu.children ? (
-                      <>
-                        {menu.children.map((child: menuType) => {
-                          return (
-                            <ListItemButton
-                              sx={{ py: 0.5, px: 1 }}
-                              to={child.href}
-                              component={Link}
-                              key={child.title ? child.id : menu.subheader}
-                            >
-                              <ListItemText
-                                primary={child.title}
-                                secondary={child.href}
-                                sx={{ my: 0, py: 0.5 }}
-                              />
-                            </ListItemButton>
-                          );
-                        })}
-                      </>
-                    ) : (
-                      ""
-                    )}
-                  </Box>
-                );
-              })}
-            </List>
-          </Box>
-        </Box>
       </Dialog>
     </>
   );
